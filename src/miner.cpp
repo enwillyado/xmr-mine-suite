3f8ffa742a6a3fcaa7b7b1a8b64ea81aca150eac
enWILLYado <enwillyado@hotmail.com>
2018-05-30 08:03:57 +0200

    Update miner protocol
    S (START)
    M (MATCHED)
    E (END)

--- src/miner.cpp
+++ src/miner.cpp
@@ -1,24 +1,5 @@
-/* XMRig
- * Copyright 2010      Jeff Garzik <jgarzik@pobox.com>
- * Copyright 2012-2014 pooler      <pooler@litecoinpool.org>
- * Copyright 2014      Lucas Jones <https://github.com/lucasjones>
- * Copyright 2014-2016 Wolf9466    <https://github.com/OhGodAPet>
- * Copyright 2016      Jay D Dee   <jayddee246@gmail.com>
- * Copyright 2016-2017 XMRig       <support@xmrig.com>
- *
- *
- *   This program is free software: you can redistribute it and/or modify
- *   it under the terms of the GNU General Public License as published by
- *   the Free Software Foundation, either version 3 of the License, or
- *   (at your option) any later version.
- *
- *   This program is distributed in the hope that it will be useful,
- *   but WITHOUT ANY WARRANTY; without even the implied warranty of
- *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
- *   GNU General Public License for more details.
- *
- *   You should have received a copy of the GNU General Public License
- *   along with this program. If not, see <http://www.gnu.org/licenses/>.
+/* MINER
+ * Only mine a job [$1], iterate nonce target [$2] from start [$3=0] to end [$4=-1].
  */
 
 #include "net/Job.h"
@@ -86,6 +67,8 @@ int main(int argc, char** argv)
 		return 2;
 	}
 
+	std::cout << "S:" << std::hex << std::setw(8) << std::setfill('0') << *job.nonce() << ";" << std::endl;
+
 	// Start hashing
 	//
 	while(true)
@@ -94,11 +77,13 @@ int main(int argc, char** argv)
 		{
 			// Share match
 			//
-			std::cout << "Matched: " << std::hex << std::setw(8) << std::setfill('0') << *job.nonce() << std::endl;
+			std::cout << "M:" << std::hex << std::setw(8) << std::setfill('0') << *job.nonce() << ";" << std::endl;
 		}
 
 		++(*job.nonce());
 	}
+
+	std::cout << "E:" << std::hex << std::setw(8) << std::setfill('0') << *job.nonce() << ";" << std::endl;
 
 	return 0;
 }
