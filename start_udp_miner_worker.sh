d4d844dbaab3a96003eede0acde7fa575b96e9b8
enWILLYado <enwillyado@hotmail.com>
2019-04-05 19:46:09 +0200

    Rename scripts

--- start_udp_miner_worker.sh
+++ start_udp_miner_worker.sh
@@ -3,21 +3,21 @@ source vars.sh
 
 function kill_and_exit {
 	wget -q -O - http://$SERVER_HOST:$SERVER_PORT/xmrig/proxy?q=end
-	./udp_miner_worker_kill.sh
+	./killall_udp_miner_workers.sh
 	echo " exit ..."
 	exit 0
 }
 
 trap kill_and_exit SIGINT SIGTERM
 
-./udp_miner_worker_kill.sh
+./killall_udp_miner_workers.sh
 
 wget -q -O - http://$SERVER_HOST:$SERVER_PORT/xmrig/proxy?q=start\&port=$UDP_PORT
 
 while :
 do
 	nc -n -u -l $UDP_PORT -w0 > job
-	./udp_miner_worker_kill.sh
+	./killall_udp_miner_workers.sh
 	PARAMS=`cat job`
 	rm job
 	echo "Job: "$PARAMS
