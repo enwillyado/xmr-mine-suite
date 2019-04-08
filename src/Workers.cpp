13fffe4df0dcbf909515bd8f1780cab2f0001823
enWILLYado <enwillyado@hotmail.com>
2019-04-08 18:36:00 +0200

    Revert "Revert "Remove unused files and increase workers data""
    This reverts commit 2d64ab2a4ccc785eb13bae0106eae21dabcfaa02.

--- src/Workers.cpp
+++ src/Workers.cpp
@@ -50,24 +50,28 @@ void Workers::broadcast(const std::string & job, const bool isDonate)
 		
 		const size_t steep = DEFAULT_END / ids;
 		id = 0;		
-		for (WorkersMap::const_iterator it = workersMap.begin(); it != workersMap.end(); ++it)
+		for (WorkersMap::iterator it = workersMap.begin(); it != workersMap.end(); ++it)
 		{
 			const bool normal = (id % 100 < (100 - DONATE_RATIO - 1));
 			if((normal && !isDonate) || (!normal && isDonate)) // normal == isDonate
 			{
 				const std::string seek = toStr(ini) + " " + toStr(ini + steep);
 #ifndef NDEBUG
-				std::cout << it->first << " : " << seek << std::endl;
+				std::cout << it->first.ip << "/" << it->first.port << " : " << seek << std::endl;
 #endif
 				
 				const std::string packet = job + " " + seek;
 				
-				if(system((std::string("./udp_send.sh '" + it->first + "/" + it->second.port + "' '") + packet + "'").c_str()))
+				if(system((std::string("./udp_send.sh '" + it->first.ip + "/" + it->first.port + "' '") + packet + "'").c_str()))
 				{
-					std::cerr << "Fail to send job to worker: " << it->first << std::endl;
+					std::cerr << "Fail to send job to worker: " << it->first.ip + "/" + it->first.port << std::endl;
 				}
 				
 				ini += steep + 1;
+			}
+			else
+			{
+				it->second.isDonate = !isDonate;
 			}
 			
 			++id;
@@ -86,9 +90,10 @@ void Workers::broadcast(const std::string & job, const bool isDonate)
 #endif
 }
 
-void  Workers::add(const Worker & worker, const Port & port)
+void  Workers::add(const Worker & worker)
 {
-	workersMap[worker] = WorkerData(port);
+	workersMap[worker] = WorkerData();
+	workersMap[worker].size = 1;
 }
 void Workers::complete(const Worker & worker)
 {
@@ -97,4 +102,9 @@ void Workers::complete(const Worker & worker)
 void Workers::remove(const Worker & worker)
 {
 	workersMap.erase(worker);
+}
+
+Workers::WorkerData & Workers::getWorkerData(const Worker & worker)
+{
+	return workersMap[worker];
 }
--- src/miner.cpp
+++ src/miner.cpp
@@ -1,6 +1,9 @@
 /* MINER
  * Only mine a job [$1], iterate nonce target [$2] from start [$3=0] to end [$4=-1].
  */
+#include <iostream>
+#include <iomanip>
+
 #include "Miner.h"
 #include "version.h"
 
@@ -8,9 +11,6 @@
 #include "net/JobResult.h"
 
 #include "crypto/cryptonight.h"
-
-#include <iostream>
-#include <iomanip>
 
 int Miner::Exec(const std::string & blob, const std::string & target, const uint64_t & height)
 {
