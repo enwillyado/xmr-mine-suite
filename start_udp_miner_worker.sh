151d1796c0793995319638f982e04a6cdc386f45
enWILLYado <enwillyado@hotmail.com>
2019-04-09 12:52:18 +0200

    Send end in foreground

--- start_udp_miner_worker.sh
+++ start_udp_miner_worker.sh
@@ -5,9 +5,9 @@
 source vars.sh
 
 function kill_and_exit {
-	wget -q -O - http://$WORKERS_HOST:$WORKERS_PORT/xmrig/proxy?\&q=end\&port=$WORKER_UDP_PORT\& &
-	./killall_udp_miner_workers.sh
 	echo " exit ..."
+	./killall_udp_miner_workers.sh
+	wget -q -O - http://$WORKERS_HOST:$WORKERS_PORT/xmrig/proxy?\&q=end\&port=$WORKER_UDP_PORT\&
 	exit 0
 }
 
@@ -23,6 +23,6 @@ do
 	./killall_udp_miner_workers.sh
 	PARAMS=`cat job`
 	rm job
-	echo "Job: "$PARAMS
+	echo "Job received: "$PARAMS
 	./miner_worker.exe $PARAMS && wget -q -O - http://$WORKERS_HOST:$WORKERS_PORT/xmrig/proxy?\&q=complete\&port=$WORKER_UDP_PORT\& &
 done
