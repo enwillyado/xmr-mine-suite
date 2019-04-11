0dcb068a3b1aa724bb75c62ba392736eead27a4e
enWILLYado <enwillyado@hotmail.com>
2019-04-11 19:12:09 +0200

    Upgrade UNIX build.

--- build.sh
+++ build.sh
@@ -42,11 +42,11 @@ ar rcsvouU miner.a  cryptonight.o cryptonight_v.o cryptonightR_template.o c_blak
 g++ $FLAGS_BASIC $FLAGS_COMPILER $FLAGS_COMPILER_CPP -c -I../src/ ../src/3rdparty/w_tcp/tcpclient.cpp ../src/3rdparty/w_tcp/tcpserver.cpp ../src/3rdparty/w_sistema/w_sistema_thread_comun.cpp ../src/3rdparty/w_sistema/w_sistema_thread_unix.cpp  ../src/3rdparty/w_sistema/w_sistema_thread_win.cpp 
 ar rcsvouU 3rdparty.a  tcpclient.o tcpserver.o w_sistema_thread_comun.o w_sistema_thread_unix.o w_sistema_thread_win.o
 
-g++ $FLAGS_BASIC $FLAGS_COMPILER $FLAGS_COMPILER_CPP -c -I../src/ ../src/minerApp.cpp ../src/minerWorker.cpp
-g++ $FLAGS_BASIC $FLAGS_COMPILER $FLAGS_COMPILER_CPP -c -I../src/ ../src/Finder.cpp ../src/Workers.cpp ../src/finderApp.cpp
+g++ $FLAGS_BASIC $FLAGS_COMPILER $FLAGS_COMPILER_CPP -c -I../src/ ../src/Finder.cpp ../src/Workers.cpp
+g++ $FLAGS_BASIC $FLAGS_COMPILER $FLAGS_COMPILER_CPP -c -I../src/ ../src/minerApp.cpp ../src/workerApp.cpp ../src/finderApp.cpp
 
 g++ $FLAGS_LINKER minerApp.o miner.a -o ../miner.exe
-g++ $FLAGS_LINKER minerWorker.o miner.a -o ../miner_worker.exe
+g++ $FLAGS_LINKER workerApp.o miner.a -o ../miner_worker.exe
 g++ $FLAGS_LINKER -pthread -lpthread Finder.o Workers.o finderApp.o 3rdparty.a -o ../finder_app.exe
 
 cd ..
